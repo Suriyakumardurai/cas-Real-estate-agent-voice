b48b8709cb79b435dc3e35179489a6b1286af5b7
suriya <suriyakumardurai.sk.in@gmail.com>
2026-04-23 16:50:34 +0530

    fixed

--- main.py
+++ main.py
@@ -183,4 +183,5 @@ if __name__ == "__main__":
 
     app.state.testing = args.test
 
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
